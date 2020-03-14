6571bf431c5664f975262e1b95cadfe1e905923e
Anton Smirnov <antosha-85@yandex.ru>
2020-03-14 10:39:00 -0600

    Created index.js file and main application funciton that includes three routes Main, Login, Signup and created those 3 empty files

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,16 +1,35 @@
 import React from "react";
+// import Header from './components/Header.js'
+// import Footer from './components/Footer.js'
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 import "components/Application.scss";
 
-export default function Application(props) {
-  return (
-    <main className="layout">
-      <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
-      </section>
-      <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-      </section>
-    </main>
-  );
+export default function Application() { 
+  const userId = sessionStorage.getItem('userId')
+
+    return (
+        <Router>
+          <div className="App" >
+            {/* < Header />                       */}
+              <Switch>           
+                <Route exact path="/" >
+                  < MainPage />
+                </Route>               
+                <Route path="/login">
+                {userId ? <Redirect to="/" /> : <Login />}
+                </Route>               
+                <Route path="/signup">
+                {userId ? <Redirect to="/" /> : <Signup />}
+                </Route>
+              </Switch>  
+            {/* < Footer />  */}
+          </div>
+        </Router>
+    );  
 }
+
+    
