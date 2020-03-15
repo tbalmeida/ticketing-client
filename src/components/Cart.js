c0819501a1e0af58700dfe36ee7f6007ffe08d45
Anton Smirnov <antosha-85@yandex.ru>
2020-03-14 21:59:42 -0600

    Modified cart and order pages, included bootstrap

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,6 +5,49 @@ export default function Cart() {
         <div>
             <h1>Cart</h1>
             <p>Product Infomation</p>
+<section class="cart-show">
+
+
+<div class="panel panel-default items">
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th colspan="2">Product</th>
+        <th>Unit Price</th>
+        <th>Quantity</th>
+        <th>Price</th>
+      </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <td>Image1</td>
+        <td>Some event1</td>
+        <td>20CAD</td>
+        <td>2</td>
+        <td>40CAD</td>
+      </tr>
+        <tr>
+        <td>Image2</td>
+        <td>Some event2</td>
+        <td>30CAD</td>
+        <td>3</td>
+        <td>90CAD</td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <tr>
+        <th colspan="4">
+          TOTAL: 
+        </th>
+        <th>
+          130CAD  
+        </th>
+      </tr>
+    </tfoot>
+  </table>
+</div>
+
+</section>
             <p><Link to='/'>
                 Go Home
             </Link>
@@ -17,3 +60,5 @@ export default function Cart() {
 
     )
 }
+
+
--- src/components/Order.js
+++ src/components/Order.js
@@ -1,15 +1,51 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 export default function Order() {
     return (
         <div>
-            <h1>Ticket(Receipt)</h1>
-            <p>You paid x amount</p>
-            <p><Link to='/'>
-                Go Home
-            </Link>
+            <h1>Cart</h1>
+            <p>Product Infomation</p>
+            <section class="cart-show">
+                <div class="panel panel-default items">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th colspan="2">Product</th>
+                                <th>Unit Price</th>
+                                <th>Quantity</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Image1</td>
+                                <td>Some event1</td>
+                                <td>20CAD</td>
+                                <td>2</td>
+                                <td>40CAD</td>
+                            </tr>
+                            <tr>
+                                <td>Image2</td>
+                                <td>Some event2</td>
+                                <td>30CAD</td>
+                                <td>3</td>
+                                <td>90CAD</td>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th colspan="4">TOTAL:</th>
+                                <th>130CAD</th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </section>
+            <p>Thank you for your order!
+                Email used for this order is "blabla@gmail.com"</p>
+            <p>
+                <Link to="/">Go Home</Link>
             </p>
         </div>
-
-    )
+    );
 }
