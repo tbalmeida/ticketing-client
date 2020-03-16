3deff7155eba3024daa1931c4639fb136d2f56da
Anton Smirnov <antosha-85@yandex.ru>
2020-03-16 17:16:10 -0600

    Added state constants for users, events, venues and going to test axios

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,22 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+function MainPage() {
+    const [users, setUsers]=useState([]);
+    const [events, setEvents]=useState([]);
+    const [venues, setVenues]=useState([]);
 // import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
     const eventsArr = [{ event:'Event1', id: 1}, { event:'Event2', id: 2}, { event:'Event3', id: 3}]
-function MainPage() {
+
+
+
+
     return (
-        <>
-        {/* <div>
-            <h1>Main page</h1>
-            <p>Hello world!</p>
-            <ul>
-                <Link to='/login'>
-                    <li>Login</li>
-                </Link>
-                <Link to='/Signup'>
-                    <li>Signup</li>
-                </Link>
-            </ul>
-        </div> */}
+       <>
         <div>
             <h2>List of Events</h2>
             <ul>
