d8364a5ec35c9dc253da675dbae3cf1f6b3cd7ca
Anton Smirnov <antosha-85@yandex.ru>
2020-03-15 19:13:28 -0600

    Updated index.js file withthe new primary color, created a theme I found on the Internet and applied normalizer for react called CssBaseLine

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from 'react-router-dom';
-
+import { BrowserRouter } from "react-router-dom";
+import { createMuiTheme } from '@material-ui/core/styles';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import "index.scss";
 
 import Application from "components/Application";
 
-ReactDOM.render(
-    <BrowserRouter>
-        <Application />
-    </BrowserRouter>,
+const theme = createMuiTheme({  palette: {    primary: { main: '#ff5826', secondary: '#ff6795'  }}}) //default primary and secondary color of font The theme specifies the color of the components, darkness of the surfaces, level of shadow, appropriate opacity of ink elements, etc.
 
-document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <MuiThemeProvider theme={theme}> {/* creating a new theme and using the theme from above */}
+      <CssBaseline /> {/* it's like a normalization.js file */}
+      < Application />
+    </MuiThemeProvider>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
