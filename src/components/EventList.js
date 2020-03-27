d52910489c6339e190f89b7d88ed62bd713092ee
Anton Smirnov <antosha-85@yandex.ru>
2020-03-27 12:38:46 -0600

    Cleaned EventList.js

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -2,10 +2,7 @@ import React, { useState, useEffect, useContext, Fragment } from "react";
 import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardMedia from "@material-ui/core/CardMedia";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import useStyles from "./EventListStyles.js";
@@ -19,7 +16,7 @@ import Theatre from "./img/theatre.png";
 import Golden from "./img/golden-ticket.jpg";
 import BackspaceSharpIcon from "@material-ui/icons/BackspaceSharp";
 import moment from "moment";
-import { convertDuration, convertTime } from "components/EventInfo";
+import { convertTime } from "components/EventInfo";
 import AddShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
 export default function EventList({ eventData, addToCart }) {
