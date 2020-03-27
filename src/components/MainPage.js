90588e91f916fba9350bfed248590b9d52a7e05d
Anton Smirnov <antosha-85@yandex.ru>
2020-03-27 12:47:11 -0600

    Cleaned MainPage.js

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,50 +1,23 @@
-import React, {useState, useEffect} from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import EventList from './EventList';
+import React from "react";
+import axios from "axios";
+import EventList from "./EventList";
 
 export function getData(url) {
-  return axios.get(`${process.env.REACT_APP_API_URL}/api/1.0${url}`)
+    return axios.get(`${process.env.REACT_APP_API_URL}/api/1.0${url}`);
 }
 
-
-function MainPage({events, addToCart}) {
-  
-//     const [users, setUsers]=useState([]);
-//     const [events, setEvents]=useState([]);
-//     const [venues, setVenues]=useState([]); 
-// // import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
-//     // const eventsArr = [{ event:'Event1', id: 1}, { event:'Event2', id: 2}, { event:'Event3', id: 3}]
-//     useEffect(() => {
-//         Promise.all([
-            
-//          getData('/events'),
-//          getData('/venues'),
-//         ])
-//           .then((all) => {
-          
-//             const [events, venues] = all;
-//             setEvents(events.data);
-//             setVenues(venues.data);
-//           })
-//           .catch(err => {
-//             console.error('ERRRROROROROR', err)
-//           })
-//       }, [])
-
-
+function MainPage({ events, addToCart }) {
     return (
-       <>
-        <div>
-            {/* <h2>List of Events</h2> */}
-            <ul>
-                <li ><EventList addToCart={addToCart} eventData={events}/></li>
-            </ul>
-        </div>
-        {/* <input type="Search" placeholder="type in events you want to find"></input> */}
+        <>
+            <div>
+                <ul>
+                    <li>
+                        <EventList addToCart={addToCart} eventData={events} />
+                    </li>
+                </ul>
+            </div>
         </>
-
-    )
+    );
 }
 
 export default MainPage;
