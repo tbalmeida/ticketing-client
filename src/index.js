fcac8fc40246f0fdd18a5d5080e585b77ec9b9a0
Anton Smirnov <antosha-85@yandex.ru>
2020-03-27 20:05:56 -0600

    Created a netlify.toml file and added if statement for index.js

--- src/index.js
+++ src/index.js
@@ -9,11 +9,16 @@ import {Elements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
 import GlobalStyles from "components/prebuilt/GlobalStyles";
 import Application from "components/Application";
+import axios from 'axios';
 
 const stripePromise = loadStripe("pk_test_Ai6UIyP2tbBXxsisSjUcXCKm00UfGStLmk");
 // const stripePromise = loadStripe(process.env.PUBLISHABLE_KEY);
 
-// 
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
 const theme = createMuiTheme({  palette: {    primary: { main: '#ff5826', secondary: '#ff6795'  }}}) //default primary and secondary color of font The theme specifies the color of the components, darkness of the surfaces, level of shadow, appropriate opacity of ink elements, etc.
 const App = () => (
   <>
